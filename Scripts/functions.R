28139f2037818af7430eb37b8ba971512acafaa5
Lcartwright94 <41172627+Lcartwright94@users.noreply.github.com>
2019-03-29 10:13:42 +1100

    Update functions.R

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -1,3 +1,18 @@
+# Reproducible code for "Bayesian atmospheric tomography for detection and quantification of methane emissions: Application to data from the 2015 Ginninderra release experiment​" by Cartwright et al.
+# Copyright (c) 2019 Laura Cartwright
+# Author: Laura Cartwright (lcartwri@uow.edu.au)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
 ###### Find Gamma params
 
 Findalphabeta_gamma <- function(pars,p5,p95) {
